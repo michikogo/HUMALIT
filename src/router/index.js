cc1118d24b7d37a28c0237eff6d0a9c7ae7e3d8a
michikogo <37466954+michikogo@users.noreply.github.com>
2021-01-31 00:29:51 +0800

    fixed some routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import { publicPath } from "../../vue.config";
 
 import Home from "../views/Home.vue";
 import MainText from "../views/MainText.vue";

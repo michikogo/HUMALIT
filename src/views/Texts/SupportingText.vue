7da3d61d46df41f807326088a6c53dae80eb1350
michikogo <37466954+michikogo@users.noreply.github.com>
2021-02-01 22:17:22 +0800

    fixed supporting text

--- src/views/Texts/SupportingText.vue
+++ src/views/Texts/SupportingText.vue
@@ -6,45 +6,247 @@
       </b-col>
       <b-col class="content-pading">
         <b-row class="content-style">
-          <!-- Title -->
-          <h3>Culture: Being Forgotten? & The Main RRL</h3>
+          <b-col>
+            <!-- Title -->
+            <h3>Supporting Texts</h3>
 
-          <!-- Context -->
-          <h5 class="video-analysis">Analysis</h5>
-          <span>
-            The text discusses how most Filipino’s of today’s generation have
-            forgotten their own culture and have changed due to technological
-            advancements and colonial mentality of the Philippine Society. It
-            talks about how culture is not just about the arts but also the
-            society’s way of thinking and modes of life. The text ends strong by
-            asking the reader to stop mimicking other cultures and instead
-            rebirth our own culture as Filipinos. What is interesting about this
-            topic is that it uses the word mimicking specifically, and mimicking
-            would entail copying every single aspect. As the Philippines have
-            been colonized by many countries, some leaving more of an impact
-            than others, it’s impossible to say that we must abolish all
-            “borrowed” culture norms from the other countries. This is because
-            our culture what it is today, is a mixture of many different
-            cultures, this can be seen in our cuisine, language etc. The article
-            only wishes to further drive the point that our true native culture
-            is being lost in the borrowed culture and we shouldn’t be mimicking
-            instead building upon them.
-          </span>
-          <!-- Context - Reference -->
-          <h5 class="video-analysis">Reference</h5>
-          <b-link
-            href="https://uglywriters.com/2018/01/30/culture-being-forgotten/"
-            class="main-link"
-          >
-            CULTURE: Being Forgotten?</b-link
-          >
-          <br />
-          <b-link
-            href="https://notforselle.wordpress.com/2019/05/14/how-can-ph-narratives-of-antiquity-be-made-relevant-to-contemporary-ph-life-culture-and-concerns/?fbclid=IwAR2pgHwUB9HxuEPZLkKAfYOZGs_Twpm3Ry9DOEvFZ7yNBZjIiiBSkUHqI-w"
-            class="main-link"
-            >How can PH narratives of antiquity be made relevant to contemporary
-            PH life, culture and concerns?</b-link
-          >
+            <!-- Supporting text 1 - Text -->
+            <h5 class="video-analysis" id="text1">
+              <b-link
+                href="https://uglywriters.com/2018/01/30/culture-being-forgotten/"
+                class="main-link"
+              >
+                CULTURE: Being Forgotten?</b-link
+              >
+            </h5>
+            <span>
+              When we talk about “culture” we often mean intellectual and
+              creative products, including literature, music, drama, and
+              painting. Another use of “culture” is to describe the beliefs and
+              practices of another society, particularly where these are seen as
+              closely linked with tradition or religion. But culture is more
+              than that. Culture is part of the fabric of every society,
+              including our own. It shapes “the way things are done” and our
+              understanding of why this should be so.
+              <br />
+              <br />
+              “Culture… is… the whole complex of distinctive spiritual,
+              material, intellectual and emotional features that characterize a
+              society or a social group. It includes not only arts and letters
+              but also modes of life, the fundamental rights of the human being,
+              value systems, traditions, and beliefs.” Societies and cultures
+              are not static. They are living entities that are continually
+              being renewed and reshaped.
+              <br />
+              <br />
+              Change is shaped by many factors. Cultural change occurs as
+              communities and households respond to social and economic shifts
+              associated with globalization, new technologies, environmental
+              pressures, armed conflict, development projects, etc. Change also
+              results from deliberate efforts to influence values through
+              changes in the law or government policy, often due to pressure
+              from civil society. There are many examples of efforts to
+              influence attitudes about race relations, the rights of workers
+              and the use of the environment, to name three areas in which
+              cultural values shape behavior.
+              <br /><br />
+              New cultural definitions are formed through a process in which
+              some segments of society promote change through advocacy and
+              example, while others resist it. In other words, societies are not
+              homogeneous and no assumptions can be made about a consensus on
+              “cultural values.” Cultural values are continually being
+              reinterpreted in response to new needs and conditions. Some values
+              are reaffirmed in this process, while others are challenged as no
+              longer appropriate.
+              <br /><br />
+              But most of the Filipinos nowadays have forgotten our culture, due
+              to the development of new technologies; people of today are too
+              much dependent on it, that’s why they weren’t able to take a look
+              back on their cultures and traditions. Like Larong Pinoy which is
+              popular during “Baby boomers and Generation X’’. Unlike now at the
+              year of Millennial, they are embracing the high-tech gadgets,
+              computer, etc.
+              <br /><br />
+              We as a student, a Filipino and a child of this nation wanted to
+              inform you guys that we should bring back those cultures that
+              disappear during modern development. Let us focus on the
+              enrichment of our dying cultures, for the sake of the next
+              generation who would inherit the cultures that we value and also
+              let them experience what we had experience, as a part of molding
+              them to be good members of our growing community.
+              <br /><br />
+              Stop adopting and mimicking the cultures and traditions of others,
+              instead find ways to help with the rebirth of our own cultures in
+              the mind, heart, and souls of every Filipinos all over the world.
+            </span>
+
+            <!-- Context -->
+            <h5 class="video-analysis" id="analysis1">Analysis</h5>
+            <span>
+              The text discusses how most Filipino’s of today’s generation have
+              forgotten their own culture and have changed due to technological
+              advancements and colonial mentality of the Philippine Society. It
+              talks about how culture is not just about the arts but also the
+              society’s way of thinking and modes of life. The text ends strong
+              by asking the reader to stop mimicking other cultures and instead
+              rebirth our own culture as Filipinos. What is interesting about
+              this topic is that it uses the word mimicking specifically, and
+              mimicking would entail copying every single aspect. As the
+              Philippines have been colonized by many countries, some leaving
+              more of an impact than others, it’s impossible to say that we must
+              abolish all “borrowed” culture norms from the other countries.
+              This is because our culture what it is today, is a mixture of many
+              different cultures, this can be seen in our cuisine, language etc.
+              The article only wishes to further drive the point that our true
+              native culture is being lost in the borrowed culture and we
+              shouldn’t be mimicking instead building upon them.
+            </span>
+          </b-col>
+        </b-row>
+
+        <b-row class="content-style">
+          <b-col>
+            <!-- Supporting text 1 - Text -->
+            <h5 id="text2">
+              <b-link
+                href="https://notforselle.wordpress.com/2019/05/14/how-can-ph-narratives-of-antiquity-be-made-relevant-to-contemporary-ph-life-culture-and-concerns/?fbclid=IwAR2pgHwUB9HxuEPZLkKAfYOZGs_Twpm3Ry9DOEvFZ7yNBZjIiiBSkUHqI-w"
+                class="main-link"
+              >
+                How can PH narratives of antiquity be made relevant to
+                contemporary PH life, culture and concerns?
+              </b-link>
+            </h5>
+            <span>
+              It’s such a fascinating fact that there is no need to extensively
+              rummage through the depths of social media to come across
+              commentaries such as the “cheap version of (a foreign product)”
+              referring to something that is Filipino-made. What makes it even
+              more peculiar is that these words often came from people who are
+              Filipinos themselves; as if they feel no love and connection, as
+              if they do not feel the need to support their own. What has gone
+              wrong, one may ask. Why are Filipinos quick to label a
+              locally-made film baduy? Why is it often referred to as a mere
+              version of a Western’s? Why is it even compared to such in the
+              first place? Maybe that’s it—where it all went wrong. Filipinos
+              use a standard that is not theirs as a basis to assess what is
+              their own, resulting to them failing to appreciate and to take
+              pride on what is theirs. They find it hard to grow some love for
+              their own country because they are not aware: of what it truly is,
+              of how much of a beauty it holds, of who the Filipino people truly
+              are, and of why they are how they are.
+              <br />
+              <br />
+              Cruz Lucero, in her essay, explains the Filipino nature of
+              continuously searching for something that deals with the order of
+              the universe; that which the Western figures could have lacked.
+              This could be seen in various Filipino literary texts. Budjette
+              Tan and Kajo Baldismo’s Trese: Thirteen Stations allows us to have
+              a peek on the richness of the Filipino culture as it mentions the
+              different beliefs the Filipinos have, particularly the myths and
+              the belief on the existence of supernatural beings who are said to
+              be in control of the cosmos. Nikki Alfar’s The Secret History of
+              Persya and Albanya, on the other hand, texhibits a part of our
+              culture and some of the Filipino characteristics manifested by the
+              characters. Such are great examples of literary works that can
+              give the Filipinos a clearer view of their cultural identity
+              because it is where it starts. To develop their love for their
+              country, they must first immerse themselves in it. For being
+              conscious of their own is also a way of fueling the heart that
+              burns of love for their country; of nudging the Filipino people to
+              shift their gaze from facing the west side because they have
+              something that is even more beautiful; something they can actually
+              relate to. This is the importance of the Filipino narratives of
+              antiquity. Through this, Filipinos today can trace back their
+              roots to where they can find the foundation of their identity. For
+              them to better understand the situation of today’s society as it
+              gives us a context of how it came to be.
+              <br />
+              <br />
+              The many years of being under an external power can be blamed for
+              what seems to be historical amnesia as it is one thing the
+              colonizers have left its former colonies. They have made the
+              latter into something that is still dependent on them even in the
+              present times, a mentality that has since then been ingrained in
+              their minds. The Philippines isn’t an exception to this. Filipinos
+              have been compelled to let go of their pre-colonial traditions
+              calling it a process of civilizing a nation. As a result, they
+              started to have a blurred vision of their own identity and have
+              started to seek for a foreign standard to explain their own. It
+              may seem difficult now to sort what is originally theirs and what
+              is brought forth by a foreign power but movement towards
+              Filipinization, though gradual, has already commenced. With the
+              marriage between Filipino culture and its then-destructive
+              husband, the foreign influence, Filipinos did well with
+              harmonizing the two: sorting them by taking in the good traits
+              they can inherit and kicking out the ones that suffocate them or
+              turning it into something that caters more to the Filipinos,
+              owning it even. This can be seen in occasions such as the
+              celebration of Christmas; how they would retain Spanish traditions
+              such as the Noche Buena but now in a more Filipino-style.
+              Similarly, Filipinos should also keep their traditions but
+              filtering it in away that is still applicable to their lives.
+              Amador Daguio’s The Wedding Dance shows us the character of a
+              Filipino wanting to conquer the wave and start a social change but
+              got caught in a crossroad as she still gives importance to their
+              traditions. As one may see from what has been mentioned, literary
+              works as such are a great way to be more socially and historically
+              conscious. This just proves how influential writers can be as they
+              have the ability to preserve and immortalize traditions and
+              cultures by re-telling them. They have the platform to mobilize
+              readers to start developing love for their country as it is a huge
+              step into solving a country’s problems. It is, then, every
+              reader’s job to support this as it is their own. Progress might be
+              a collective action, but it starts from the inside.
+            </span>
+
+            <!-- Context -->
+            <h5 class="video-analysis" id="analysis2">Analysis</h5>
+            <span>
+              The second text supplements the things mentioned in the first
+              text, stating that Filipinos have forgotten what is theirs due to
+              foreign powers imposing their process of “civilization” towards
+              the ancient Filipinos, who already had a civilization in place. It
+              mentions how we always tend to look outwards to be able to compare
+              what we have to what they have. We always place what other powers
+              have on a pedestal, and compare what we have to that, so it seems
+              like whatever we make will always be inferior, or a cheap knock
+              off. The text also discusses how we took whatever good traits or
+              good things the foreign powers have given us, and forgot the ones
+              we deem as harmful, in an attempt to create something of our own.
+              This relates to how much we view our present-day culture as a
+              melting pot of cultures, because we were colonized multiple times,
+              and from those, we grabbed a part of their culture, and tried to
+              integrate those “good things” into our own. It is worth mentioning
+              that this text mentions multiple works, such as Amador Daguio’s
+              The Wedding Dance, where a Filipino character wants to change
+              society, but is in conflict with traditions, and mentions how
+              writers and their literary works can play a vital role in
+              preserving what our traditions were, as these literary works can
+              serve as “immortalizing” tools, so that we may never forget to go
+              back to our roots, in our pursuit of modern day globalization.
+            </span>
+          </b-col>
+        </b-row>
+        <b-row class="content-style">
+          <b-col>
+            <h5 id="synthesis">Synthesis</h5>
+            <span>
+              These two texts showcase what
+              <router-link to="/marcelo-del-pilar">
+                <span class="author-color">Del Pilar’s</span>
+              </router-link>
+              text unconsciously does, and that is the process of taking
+              something from a foreign power, and turn it into something more
+              Filipino. Del Pillar’s text was mainly for satire, or to insult
+              the prayers being taught to us by the spaniards. This insult to
+              the primary tool of the Spaniards for the colonization of the
+              Philippines shows the extent of how much we can turn their tools
+              against them. To an extent, Del Pillar’s text may have awoken the
+              senses of some of our country men, opening their eyes to the
+              reality that they face, and this turn around through his work made
+              that possible.
+            </span>
+          </b-col>
         </b-row>
       </b-col>
     </b-row>
@@ -58,7 +260,22 @@ export default {
   components: { SideNav },
   data() {
     return {
-      contents: [{ key: 1, name: "Text", section: "/supportingtext#text" }],
+      contents: [
+        {
+          key: 1,
+          name: "CULTURE: Being Forgotten?",
+          section: "/supportingtext#text1",
+        },
+        { key: 2, name: "Analysis", section: "/supportingtext#analysis1" },
+        {
+          key: 3,
+          name:
+            "How can PH narratives of antiquity be made relevant to contemporary PH life, culture and concerns?",
+          section: "/supportingtext#text2",
+        },
+        { key: 4, name: "Analysis", section: "/supportingtext#analysis2" },
+        { key: 5, name: "Synthesis", section: "/supportingtext#synthesis" },
+      ],
     };
   },
 };
